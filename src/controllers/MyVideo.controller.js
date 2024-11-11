edd9bf533752b56572d04904757554df1c729a0e
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-11-11 06:13:14 +0530

    added option for personal video

--- src/controllers/MyVideo.controller.js
+++ src/controllers/MyVideo.controller.js
@@ -1,4 +1,32 @@
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 
-const GetallMyvideo=asyncHandler()
+const GetallMyvideo = asyncHandler(async (req, res) => {
+    const { userLoggedIn, user } = req;
+    if (userLoggedIn == false || !user) {
+        return res.status(404).json(new ApiResponse(404, {}, "You are not authorized to access"));
+    }
+
+    const userid = new mongoose.Types.ObjectId(user?._id);
+    if (!userid) {
+        return res.status(404).json(new ApiResponse(404, {}, "You are not authorized to access"));
+    }
+
+    try {
+        const myVideo = await Video.find({ owner: userid })
+            .select("_id video_public_id thumbnail title createdAt");
+
+        if (myVideo.length === 0) {
+            return res.status(200).json(new ApiResponse(200, [], "No video found."));
+        }
+
+        return res.status(200).json(new ApiResponse(200, myVideo, "Personalized videos fetched successfully"));
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(new ApiResponse(500, {}, "Internal server error"));
+    }
+});
+export default GetallMyvideo
--- src/routes/Video.routes.js
+++ src/routes/Video.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { deleteVideo, getAllVideo, getVideoById, getVideoDetaisbyVideo_public_id, PublsihVideo, togglePublishStatus, updateVideoDetails, updateViewCount } from "../controllers/Video.controller.js";
+import { UserVerification } from "../middlewares/UserVerification.middleware.js";
+import GetallMyvideo from "../controllers/MyVideo.controller.js";
 
 
 const router = Router()
@@ -25,5 +27,6 @@ router
 router
     .route("/publicid/:video_public_id").get(getVideoDetaisbyVideo_public_id)
 router.route("/toggle/publish/:videoid").patch(verifyJWT, togglePublishStatus);
-router.route("/update-view-count/:videoid").patch( updateViewCount)
+router.route("/update-view-count/:videoid").patch(updateViewCount)
+router.route("/my/personalised-video").get(UserVerification, GetallMyvideo)
 export default router
