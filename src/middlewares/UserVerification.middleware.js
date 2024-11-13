28bb3eee1d07b710c1e0639b2d04ad0fc66ff9b0
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-11-13 17:34:18 +0530

    update to test

--- src/middlewares/UserVerification.middleware.js
+++ src/middlewares/UserVerification.middleware.js
@@ -1,42 +1,33 @@
 import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"; // Ensure JWT is imported
 
 export const UserVerification = asyncHandler(async (req, res, next) => {
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-
-    // If there's no token, set userLoggedIn to false and proceed to next middleware
-    if (!token) {
-        req.userLoggedIn = false;
-        return next();
-    }
-
     try {
-        // Verify token and extract user information
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-        // Retrieve user from the database without sensitive fields
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
-
-        // If user is not found, set userLoggedIn to false
-        if (!user) {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+        if (!token) {
             req.userLoggedIn = false;
             return next();
         }
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
 
-        // If user is found, set userLoggedIn to true and add user data to request
+        if (!user) {
+            req.userLoggedIn = false;
+            return res.status(500).json(new ApiResponse(500,{},"afh"));
+            return next();
+        }
+
+       
         req.userLoggedIn = true;
         req.user = user;
 
-        next();
+        return next();
+
     } catch (error) {
-        // Handle errors from jwt verification or database operations
-        if (error.name === 'TokenExpiredError' || error.name === 'JsonWebTokenError') {
-            req.userLoggedIn = false; // Token-related error, so set to false
-        } else {
-            console.error("Unexpected error in UserVerification middleware:", error);
-        }
+
+        req.userLoggedIn = false;
         next();
     }
 });
-
